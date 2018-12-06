67aa5b6c9fd16f6a922229495ab0bcda9e20237c
vincent040 <18620373040@163.com>
2018-12-06 23:11:12 +0900

    通用顺序表

--- 01/sequence_list.h
+++ 01/sequence_list.h
@@ -1,6 +1,29 @@
+////////////////////////////////////////////////
+//
+//  Copyright(C), 广州粤嵌通信科技股份有限公司
+//
+//  作者: Vincent Lin (林世霖)
+//
+//  微信公众号: 秘籍酷
+//  技术交流群: 260492823（QQ群）
+//  GitHub链接: https://github.com/vincent040
+//
+//  描述: 通用顺序表，支持不同类型的节点
+//  1) 对于内置类型节点，在编译时通过如下宏指定类型即可:
+//     -DSQLIST_NODE_DATATYPE="long double"
+//
+//  2) 对于自定义类型节点，还需在本头文件作显式声明，如:
+//  struct student
+//  {
+//	char *name;
+//	int   age;
+//	float score;
+//  };
+//
+////////////////////////////////////////////////
+
 #ifndef __SEQUENCE_LIST
 #define __SEQUENCE_LIST
-
 
 #ifndef SQLIST_NODE_DATATYPE
 #define SQLIST_NODE_DATATYPE int
@@ -42,3 +65,5 @@ void destroy(sqlist list);
 void sort(sqlist, bool(*comp)(datatype d1, datatype d2));
 
 #endif
+
+
